97916ac68fa06e1f186a6c6cd796615de0f82b26
abumeis <37344104+abumeis@users.noreply.github.com>
2021-01-06 13:52:40 +0100

    turn right and left

---
+++ project.js
@@ -0,0 +1,58 @@
+var grid = [
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+];
+
+var rover = {
+        direction: "N",
+        location: { x: 0, y: 0 }
+    }
+    ///va vers la gauche
+function turnLeft(rover) {
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "W";
+            break;
+        case "W":
+            rover.direction = "S";
+            break;
+        case "S":
+            rover.direction = "E";
+            break;
+        case "E":
+            rover.direction = "N";
+            break;
+    }
+    console.log(` Rover est va vers ${rover.direction}`);
+}
+console.log(turnLeft(rover))
+
+///va vers la droite 
+function turnRight(rover) {
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "E";
+            break;
+        case "E":
+            rover.direction = "S";
+            break;
+        case "S":
+            rover.direction = "W";
+            break;
+        case "W":
+            rover.direction = "N";
+            break;
+    }
+    console.log(` Rover est va vers ${rover.direction}`);
+}
+
+
+console.log(turnRight(rover))
